fbb195540916b92aa5d8581e43fe7d76cc7aa9c6
Marcin Świrek <swirek.marcin@gmail.com>
2022-09-26 17:24:54 +0200

    Updates

---
+++ precache-manifest.4fa9723e76009bf77322c43f2722d330.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6913c5dfeeb605704682",
+    "url": "/github-user-search/static/js/main.0d400e69.chunk.js"
+  },
+  {
+    "revision": "1929e8f15580a5d437fd",
+    "url": "/github-user-search/static/js/runtime~main.e5dc5a1d.js"
+  },
+  {
+    "revision": "fca893f1fb2d98edb1c0",
+    "url": "/github-user-search/static/js/2.24563afd.chunk.js"
+  },
+  {
+    "revision": "b84224acadcdf4e6a29af6dd4f8a9902",
+    "url": "/github-user-search/static/media/ArnoldRegular.b84224ac.woff"
+  },
+  {
+    "revision": "d303e90e0676af91aea12d0c78e8fe90",
+    "url": "/github-user-search/static/media/github-logo.d303e90e.png"
+  },
+  {
+    "revision": "08c03c6a24e5058b9f3556981a23b1d7",
+    "url": "/github-user-search/static/media/github.08c03c6a.png"
+  },
+  {
+    "revision": "09339da19be88d86868ac4e33a4a32c6",
+    "url": "/github-user-search/index.html"
+  }
+];
